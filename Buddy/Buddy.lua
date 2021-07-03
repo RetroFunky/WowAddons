96dc5eb7266ef09fcd94254808e0b1e43755454b
Luca <iov.luca@yahoo.ro>
2021-07-03 03:49:51 +0300

    Added my weapon swing timer Added buddy sketch

---
+++ Buddy/Buddy.lua
@@ -0,0 +1,5 @@
+-- Reminder: You promised Anca you'd ask her
+-- What's tentin quarantinos favorite word
+
+local load_message = L["Szchimer's got your back,"] .. " " 
+                    .. L["but you got to have your buddies"]
